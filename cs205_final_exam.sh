288ed14a36fa76fbc3bc7d792511b2c87ab69874
Clayton <car723@nau.edu>
2023-04-01 02:43:05 +0000

    I modified the sh final file and made a screenshot directory in my repository

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -10,3 +10,19 @@
 # The spacing and header formatting should match the above formatting description exactly.
 # There should be a comment explaining the purpose of each line in your shell script. 
 # The data file will be passed in to the script as a positional parameter and will not necessarily be called pokemon.dat. However, you can assume that any file passed to this script will be formatted exactly the way pokemon.dat is formatted.
+
+#!/bin/bash
+#Gets file as first proitional argument
+file=$1
+#Determines the total number of pokemon
+total_mons=$(awk 'END {print NR-1}' $file)
+#Calculates the average of both the HP and attack numbers of all the pokemon using awk 
+HP_Avg=$(awk '{sum+=$6} END {print sum/NR-1}' $file)
+Atk_Avg=$(awk '{sum+=$7} END {print sum/NR-1}' $file)
+#Prints the summary of the file
+echo "===== SUMMARY OF DATA FILE ====="
+echo "   File name: $file"
+echo "   Total Pokemon: $total_mons"
+echo "   Avg. HP: $HP_Avg"
+echo "   Avg. Attack: $Atk_Avg"
+echo "===== END SUMMARY ====="
